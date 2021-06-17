fea71fc7f83d4d82a277fcfd334cd8d6fcf485b9
Ranesha22 <raneshapal@gmail.com>
2021-06-17 23:53:22 +0530

    redeploy

--- server.js
+++ server.js
@@ -5,12 +5,17 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
+// app.use(express.static(__dirname + '/dist/angular'));
+
+// app.get('/*', function(req,res) => {
+    
+// res.sendFile(path.join(__dirname+'/dist/angular/index.html'));
+// });
+
 app.use(express.static(__dirname + '/dist/angular'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/angular/index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(`./dist/angular/index.html`); // load the single view file (angular will handle the page changes on the front-end)
 });
-
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
